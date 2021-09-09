fa5fd7f95df44973246a006e176b707d9d3dd0d7
AmirAbu-Almajd <amir.abualmajd99@gmail.com>
2021-09-09 11:07:04 +0200

    updating main structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
+import 'ngx-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
